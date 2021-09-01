09e64b09d93e5c592b4800dbbfed036c7a84026e
Luis Lourenço <lhal07@gmail.com>
2021-09-01 19:20:13 -0300

    Part3: Animate Platform

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private Animator m_Anim;
+    private PlayerController m_Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Anim = gameObject.GetComponent<Animator>();
+        m_Player = (PlayerController)FindObjectOfType(typeof(PlayerController));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.DetachChildren();
+        }
+    }
+}
